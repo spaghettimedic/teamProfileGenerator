3914bcbede25aaa52fe5c5568392a709ece16b2b
spaghettimedic <dsteiner247@gmail.com>
2021-12-03 15:07:54 -0500

    wrote passing tests for and created Employee object's properties and methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,19 @@
 const Employee = require('../lib/Employee');
 
 test('creates an Employee object', () => {
-  const employee = new Employee('David', '1704867', 'dsteiner247@gmail.com');
+  const employee = new Employee('David', '1704867', 'dsteiner247@gmail.com', 'Employee');
 
   expect(employee.name).toEqual(expect.any(String));
   expect(employee.id).toEqual(expect.any(String));
   expect(employee.email).toEqual(expect.any(String));
+  expect(employee.role).toEqual(expect.any(String));
+});
+
+test("gets Employee object's properties", () => {
+  const employee = new Employee('David', '1704867', 'dsteiner247@gmail.com', 'Employee');
+
+  expect(employee.getName()).toHaveProperty('name');
+  expect(employee.getId()).toHaveProperty('id');
+  expect(employee.getEmail()).toHaveProperty('email');
+  expect(employee.getRole()).toHaveProperty('role');
 });
