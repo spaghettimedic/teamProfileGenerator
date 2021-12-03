4331d577cfdad479958d92d728ceff79e33fae61
spaghettimedic <dsteiner247@gmail.com>
2021-12-03 15:20:10 -0500

    wrote passing tests for and created Engineer object's properties and methods

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,21 @@
+const Engineer = require('../lib/Engineer');
+
+test('creates an Engineer object', () => {
+  const engineer = new Engineer();
+
+  expect(engineer.name).toEqual(expect.any(String));
+  expect(engineer.id).toEqual(expect.any(String));
+  expect(engineer.email).toEqual(expect.any(String));
+  expect(engineer.role).toEqual(expect.any(String));
+  expect(engineer.github).toEqual(expect.any(String));
+});
+
+test("gets Engineer object's properties", () => {
+  const engineer = new Engineer();
+
+  expect(engineer.getName()).toHaveProperty('name');
+  expect(engineer.getId()).toHaveProperty('id');
+  expect(engineer.getEmail()).toHaveProperty('email');
+  expect(engineer.getRole()).toHaveProperty('role');
+  expect(engineer.getGithub()).toHaveProperty('github');
+});
