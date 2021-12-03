dc9d8c92ed570bcccaeed40198409891cde87344
spaghettimedic <dsteiner247@gmail.com>
2021-12-03 16:06:49 -0500

    wrote passing tests for and created Intern object's properties and methods

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,20 @@
+const Intern = require('../lib/Intern');
+
+test('creates an Intern object', () => {
+  const intern = new Intern();
+
+  expect(intern.name).toEqual(expect.any(String));
+  expect(intern.id).toEqual(expect.any(String));
+  expect(intern.email).toEqual(expect.any(String));
+  expect(intern.school).toEqual(expect.any(String));
+});
+
+test("gets Intern object's properties", () => {
+  const intern = new Intern();
+
+  expect(intern.getName()).toHaveProperty('name');
+  expect(intern.getId()).toHaveProperty('id');
+  expect(intern.getEmail()).toHaveProperty('email');
+  expect(intern.getRole()).toBe('Intern');
+  expect(intern.getSchool()).toHaveProperty('school');
+});
